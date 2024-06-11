ee71978f3599187bbf6d75551fd618df59801040
Your Name <rayliu349@gmail.com>
2024-06-10 22:51:16 -0400

    basic nav

---
+++ shouldBeWinnable/src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav className = 'flex justify-between items-center h-12 bg-white text-black relative shadow-sm font-mono'>
+            <Link to='/' className='pl-8'>
+                Should Be Winnable
+            </Link>
+            <div className='px-4 curser-pointer md:hidden'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+            </div>
+            <div className='pr-8 md:block hidden'>
+                <Link className='p-4' to="/">Home</Link>
+                <Link className='p-4' to="/menu">Menu</Link>
+                <Link className='p-4' to="/about">About</Link>
+                <Link className='p-4' to="/pog">PogChamp</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ shouldBeWinnable/src/main.tsx
@@ -0,0 +1,16 @@
+import Navbar from "./Components/Navbar"
+
+function Main() {
+
+    return (
+        <div className='pog'>
+            <Navbar />
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+
+        </div>
+    )
+}
+
+export default Main;
